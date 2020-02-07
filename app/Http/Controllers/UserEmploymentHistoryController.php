04e356b2f9e8ec328691bba46f6fad0df8496a77
Farrah <farrah.vecino@myxlog.com>
2020-02-07 11:38:02 +0800

    addedd request

--- app/Http/Controllers/UserEmploymentHistoryController.php
+++ app/Http/Controllers/UserEmploymentHistoryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserEmploymentHistoryController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $params = $request->all();
 
